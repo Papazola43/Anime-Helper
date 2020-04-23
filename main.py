34d6328e930c64395720590884f5166bb701f811
Papazola43 <63558758+Papazola43@users.noreply.github.com>
2020-04-23 23:37:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,89 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from os import system
 from time import sleep
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import re
 
 
-link = "https://kissanime.ru/Anime/"
+link = "https://kissanime.ru"
+EPISODE_BEGIN = '<table class="listing">\n                            <tbody><tr>\n                                <th width="85%">\n                                    Episode name\n                                </th>\n                                <th width="15%">\n                                    Day Added\n                                </th>\n                            </tr>\n                            <tr style="height: 10px">\n                            </tr>\n                            \n'
+EPISODE_BEGIN = re.escape(EPISODE_BEGIN).split('\\\n')
+EPISODE_BEGIN = '\s*\\\n\s*'.join(EPISODE_BEGIN)
+EPISODE_END = '</tbody></table>\n                        \n                    </div>\n                </div>\n            </div>\n'
+EPISODE_END = re.escape(EPISODE_END).split('\\\n')
+EPISODE_END = '\s*\\\n\s*'.join(EPISODE_END)
+
+def closeAd(BROWSER):
+	if len(BROWSER.window_handles) > 1:
+		BROWSER.switch_to.window(BROWSER.window_handles[1])
+		BROWSER.close()
+	BROWSER.switch_to.window(BROWSER.window_handles[0])
+	if BROWSER.current_url.find('kissanime') == -1:
+		BROWSER.execute_script("window.history.go(-1)")
+	return	
 
 def submit_idpass(BROWSER):
 	with open('idpass.txt', 'r') as f:
 		kissmanga_credentials = f.read().splitlines()
 		username_area = BROWSER.find_element_by_id('username')
+		username_area.send_keys(Keys.CONTROL, "a")
 		username_area.send_keys(kissmanga_credentials[0])
 		password_area = BROWSER.find_element_by_id('password')
+		password_area.send_keys(Keys.CONTROL, "a")
 		password_area.send_keys(kissmanga_credentials[1])
 		submit_button = BROWSER.find_element_by_id('btnSubmit')
 		submit_button.click()
+		closeAd(BROWSER)
+		if password_area == '' or username_area == '':
+			submit_idpass()
+	return
+
+def afterCaptcha(BROWSER):
+	for i in range(30):
+		if 'AreYouHuman2' in BROWSER.current_url:
+			sleep(2)
+		elif i == 30:
+			exit()
+		elif 'kissanime' in BROWSER.current_url:
+			sleep(4)
+			break
+		else:
+			continue
+
+	closeAd(BROWSER)
+
+	close_ad = BROWSER.find_element_by_class_name('glx-close.glx-close-with-text')
+	close_ad.click()
+	sleep(1)
+	play_button = BROWSER.find_element_by_class_name('vjs-big-play-button')
+	play_button.click()
+
+	closeAd(BROWSER)
+	sleep(1)
+	fullscreen_button = BROWSER.find_element_by_class_name('vjs-fullscreen-control.vjs-control')
+	fullscreen_button.click()
+	if len(BROWSER.window_handles) > 1:
 		BROWSER.switch_to.window(BROWSER.window_handles[1])
 		BROWSER.close()
 		BROWSER.switch_to.window(BROWSER.window_handles[0])
-		if BROWSER.current_url.find('kissanime') == -1:
-			BROWSER.execute_script("window.history.go(-1)")
-	return
+		fullscreen_button.click()
+
+	while True:
+		sleep(5)
+		raw_data = BROWSER.page_source
+		soup = BeautifulSoup(raw_data,'html.parser')
+		progress = soup.find('div',{'role':"slider"})['aria-valuenow']
+		if(progress == '100'):
+			break
+	try:
+		next_button = BROWSER.find_element_by_id('btnNext')
+	except NoSuchElementException:
+		keyboard.press(Key.escape)
+		keyboard.release(Key.escape)
+		return
+	next_button.click()	
+	afterCaptcha(BROWSER)
 
 def showMenu():
 	print("1. Start watching")
@@ -57,15 +121,31 @@ def startWatching():
 	print('processing....')
 	sleep(2)						
 	name_of_anime = lists[index-1]
+	#start opening chrome
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_argument("--start-maximized")
-	vid = link + name_of_anime.replace(' ','-')+'?'
+	vid = link + '/Anime/' + name_of_anime.replace(' ','-')
 	driver = webdriver.Chrome(options=chrome_options)
-	driver.get('https://kissanime.ru/login')
+	driver.get(link+'/login')
 	sleep(6)
+
 	submit_idpass(driver)
 	driver.get(vid)
 	system('cls')
+	#opening the last viewed episode
+	raw_data = driver.page_source
+	anime_list_raw = re.search(EPISODE_BEGIN+'([\s\S]*?)'+EPISODE_END, raw_data).group(1)
+	episode_iter = re.finditer('<a '+'([\s\S]*?)'+'</a>', anime_list_raw)
+	for matched_sentence in episode_iter:
+		matched_sentence = matched_sentence.group(1)
+		link_append = re.search('href="'+'([\s\S]*?)'+'" title=',matched_sentence).group(1)
+		if 'class="episodeVisited"' in matched_sentence:
+			break
+		else:
+			continue		
+	driver.get(link+link_append)
+	system('cls')
+	afterCaptcha(driver)
 	return
 
 def showList():
